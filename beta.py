29ebb458c6a17e5e4d3dd84f650810ecd878e0f0
Staryuu <50374216+Staryuu1@users.noreply.github.com>
2024-06-26 21:31:35 +0700

    Update beta.py

--- beta.py
+++ beta.py
@@ -3,8 +3,14 @@ import requests
 
 url = 'https://raw.githubusercontent.com/Staryuu1/Rejoin-main/main/rejoin-beta.py'
 
-response = requests.get(url)
-script_content = response.text
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Check for request errors
 
+    script_content = response.text
 
-exec(script_content)
+    # Execute the script in a restricted namespace
+    exec(script_content)
+
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
